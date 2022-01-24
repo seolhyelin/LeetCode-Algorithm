eb41d3c9665118d138753a550e96a54515017818
Seol HyeRin <90893364+Seoltorin@users.noreply.github.com>
2022-01-24 12:08:04 +0900

    Time: 163 ms (29.24%), Space: 39.2 MB (88.72%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -8,9 +8,7 @@ var twoSum = function(nums, target) {
     for(let i=0;i<nums.length;i++){
         for(let j=i+1;j<nums.length;j++){
             if(nums[i]+nums[j]===target){
-                // arr.push[i];
-                // arr.push[j];
-                // break;
+         
                 return arr=[i,j];
             }
         }
