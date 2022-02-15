56e16e07e1950856fa3e5ad5b65ba9d9b98ab062
Hye Lin <90893364+seolhyelin@users.noreply.github.com>
2022-02-15 13:07:30 +0900

    Time: 133 ms (40.77%), Space: 51.2 MB (10.12%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+        
+  // 이전값이 음수면, 현재의 값에 더하지 않는다.
+  // 양수일 때만, 현재 값에 더해서 저장한다. (최대 sum을 구하기 위해서)
+    nums.forEach((value,index)=>{
+        if(nums[index-1]>0){
+            nums[index]= value + nums[index-1]
+        }
+    });
+    
+    return Math.max(...nums);//배열의 가장 큰 값 return 하기
+    
+    
+
+};
