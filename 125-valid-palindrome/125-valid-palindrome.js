fa64fc21fc24fae3ef65bf40046f30accbf22268
Hye Lin <90893364+seolhyelin@users.noreply.github.com>
2022-02-23 13:40:41 +0900

    Time: 166 ms (12.50%), Space: 46.8 MB (22.33%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    const reg = /[^\w\sㄱ-힣]|[\_]/g;
+  	const regex = / /gi;
+    
+    let str=s.toLowerCase().replace(reg, ``).replace(regex,'');
+	
+  	let newStr=str.split('').reverse().join('');
+
+  if(str===newStr){
+      return true;
+  }else{
+      return false;
+  }
+};
