b0cebde20fb627f068280dae6442a02047ed8f26
Hye Lin <90893364+seolhyelin@users.noreply.github.com>
2022-03-07 17:00:46 +0900

    Time: 62 ms (88.97%), Space: 42 MB (66.91%) - LeetHub

--- 884-uncommon-words-from-two-sentences/884-uncommon-words-from-two-sentences.js
+++ 884-uncommon-words-from-two-sentences/884-uncommon-words-from-two-sentences.js
@@ -4,17 +4,20 @@
  * @return {string[]}
  */
 var uncommonFromSentences = function(s1, s2) {
-const concat = [...s1.split(' '), ...s2.split(' ')];
-
-  const obj = {};
-  for (let str of concat) {
-    obj[str] ? obj[str]++ : (obj[str] = 1);
+	const str1=s1.split(' ');
+  const str2=s2.split(' ');
+  const concat=str1.concat(str2);
+  
+  let obj={};
+  for(let word of concat){
+    obj[word] ? obj[word]++ : (obj[word]=1);
   }
-
-  const result = [];
-  for (let key in obj) {
-    if (obj[key] === 1) result.push(key);
+    
+  let result=[];
+  for(let key in obj){
+    if(obj[key]===1){
+      result.push(key);
+    }
   }
-
   return result;
 };
