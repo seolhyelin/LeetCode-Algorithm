2b2a4cba2024539a55f78efefa7c4f0c76533129
Hye Lin <90893364+seolhyelin@users.noreply.github.com>
2022-04-26 12:34:28 +0900

    Time: 94 ms (44.50%), Space: 42.9 MB (62.39%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.js
+++ 1446-consecutive-characters/1446-consecutive-characters.js
@@ -3,12 +3,11 @@
  * @return {number}
  */
 var maxPower = function(s) {
- const arr=s.split('')
 	let count=0;
   let max=0;
   
-  for(let i=0; i<arr.length;i++){
-    if(arr[i]==arr[i+1]){
+  for(let i=0; i<s.length;i++){
+    if(s[i]==s[i+1]){
       count++;
     }else {
       count++;
