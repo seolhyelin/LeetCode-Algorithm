d99438b54ccca20a62d120257ad8d3b4922693ee
Hye Lin <90893364+seolhyelin@users.noreply.github.com>
2022-07-05 11:22:00 +0900

    Time: 67 ms (82.99%), Space: 42.2 MB (54.42%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} word
+ * @param {character} ch
+ * @return {string}
+ */
+var reversePrefix = function(word, ch) {
+	const findIndex = word.indexOf(ch);
+
+	if (findIndex == -1) return word;
+
+	const reverseWords = [...word.slice(0, findIndex + 1)].reverse().join('');
+
+	return reverseWords + word.slice(findIndex + 1);
+};
