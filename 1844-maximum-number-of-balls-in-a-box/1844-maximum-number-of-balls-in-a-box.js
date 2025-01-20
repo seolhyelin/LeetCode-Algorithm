bb4fc42785a08178841f583cd2770569f618f5e8
Hye Lin <90893364+seolhyelin@users.noreply.github.com>
2025-01-20 16:17:32 +0900

    Time: 254 ms (10.45%), Space: 68.6 MB (5.97%) - LeetHub

---
+++ 1844-maximum-number-of-balls-in-a-box/1844-maximum-number-of-balls-in-a-box.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} lowLimit
+ * @param {number} highLimit
+ * @return {number}
+ */
+var countBalls = function(lowLimit, highLimit) {
+  let obj={}
+  
+    for(let i=lowLimit; i<=highLimit; i++){
+      if(0<i && i<10){
+        obj[i] = obj[i] ? obj[i]+1 : obj[i] = i
+      }else if(i>9){
+        let sum = 0
+        sum = i.toString().split("").reduce((a,b)=> a+Number(b),0)
+        obj[i] = obj[i] ? obj[i]+1 : obj[i] = sum
+      }
+    }
+    const valuesArr=Object.values(obj).sort()
+    const count = {}
+    for(let i of valuesArr){
+      count[i] = (count[i] || 0) + 1
+    }
+    return Math.max(...Object.values(count))
+};
